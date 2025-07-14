f796c3bd53b5595aede3f29595cfc954c3e9ba1f
Ailin Wuille Bille <jawbille@gmail.com>
2025-07-14 18:35:43 -0300

    Create comis.js
    Controla la carga de las comisiones y el popup de +info

---
+++ javascript/comis.js
@@ -0,0 +1,60 @@
+const grillacomis = document.getElementById("grillacomis"); // se declara el contenedor de comisiones
+
+function cargarComisiones(comisiones){ // carga de items del array de comisiones
+    comisiones.forEach(comi => {
+        const nuevaComi = document.createElement("div");
+        nuevaComi.classList = "categoria";
+       
+        const nuevaInfo1 = document.createElement("div");
+        nuevaInfo1.classList = "info";
+        nuevaInfo1.innerHTML = `<img src = "${comi.img}">`;            
+        
+        const ruta = `<img src = "${comi.img}">`;
+
+        const nuevaInfo2 = document.createElement("div");
+        nuevaInfo2.classList = "info";
+        nuevaInfo2.innerHTML = `
+        <h4>${comi.nombre}</h4>
+        <p>${comi.texto}</p>
+        <button type = "button">+ INFO</button>
+        `;
+        if(comi.id%2 === 0){ // si es par, va primero texto y luego imagen
+           nuevaComi.appendChild(nuevaInfo2); //sub div texto
+           nuevaComi.appendChild(nuevaInfo1); //sub div imagen             
+        }
+        else{ // si es impar, va primero imagen y luego texto
+            nuevaComi.appendChild(nuevaInfo1); //sub div imagen
+            nuevaComi.appendChild(nuevaInfo2); //sub div texto
+        }
+                
+        grillacomis.appendChild(nuevaComi);
+        
+        nuevaComi.getElementsByTagName("button")[0].addEventListener('click', function(){
+            mostrarMasInfo(comi); // le pasa el objeto entero del popup
+        } );
+    });
+}
+
+cargarComisiones(comisiones); // llama a la funcion agregar comisiones
+
+function mostrarMasInfo(com){
+    const fondo = document.getElementById("popupFondo"); // el fondo blurreado
+    const cruz = document.getElementsByClassName("cerrar")[0]; // la cruz de cierre del popup
+    fondo.style.display = "block";
+    const imagenPop = document.getElementById("pic"); // obtiene la ubicacion del <p>
+    imagenPop.innerHTML = `<img src = "${com.img}">`;
+    const textoPop = document.getElementById("data");
+    textoPop.innerHTML = `
+    <h4>${com.nombre}</h4>
+    <p>${com.descripcion}</p>
+    `;    
+    cruz.addEventListener('click', function(){
+        fondo.style.display = "none"; // cierra si clickea en la cruz
+    })
+    
+    window.addEventListener('click', function(e){
+        if(e.target == fondo){ // cierra si clickea en cualquier parte del popup blurreado
+            fondo.style.display = "none";
+        }
+    })
+}
